776d3bfe68cadf7b872ca9cd1461a45c07bf852d
Matthew Riddle <mriddle89@gmail.com>
2012-09-16 20:51:45 +1000

    Added sprocket vendor support

--- app.rb
+++ app.rb
@@ -8,15 +8,17 @@ end
 class App < Sinatra::Base
   set :root, File.expand_path('../', __FILE__)
   set :sprockets, Sprockets::Environment.new(root)
-  set :precompile, [ /\w+\.(?!js|css).+/, /application.(css|js)$/ ]
+  set :precompile, [ /\w+\.(?!js|css).+/, /(application|vendor).(css|js)$/ ]
   set :assets_prefix, 'assets'
   set :assets_path, File.join(root, 'public', assets_prefix)
   set :views, Proc.new { File.join(root, "app/views") }
 
   configure do
-    sprockets.append_path(File.join(root, 'assets', 'stylesheets'))
-    sprockets.append_path(File.join(root, 'assets', 'javascripts'))
-    sprockets.append_path(File.join(root, 'assets', 'images'))
+    ["assets", "vendor"].each do |path|
+      sprockets.append_path(File.join(root, path, 'stylesheets'))
+      sprockets.append_path(File.join(root, path, 'javascripts'))
+      sprockets.append_path(File.join(root, path, 'images'))
+    end
 
     sprockets.context_class.instance_eval do
       include AssetHelpers
